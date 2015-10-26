d91e0add350561f00965808c432d52dddda255d1
pdicerbo <pierluigi.dicerbo@gmail.com>
2015-10-26 16:33:30 +0100

    last commit

--- D1-exercise/mymodule.py
+++ D1-exercise/mymodule.py
@@ -9,15 +9,18 @@ class Function():
         self.codomain = np.array([])
         self.functype = "Not Specified"
 
-    def evaluate(self, func, x):
-        if(self.dim_domain == 1 and self.dim_codomain == 1):
-            self.domain = x
-            self.codomain = func(x)
-        elif(self.dim_domain == 2 and self.dim_codomain == 1):
-            # self.domain = x
-            x1, x2 = np.meshgrid(x, x)
-            self.domain = np.meshgrid(x, x)
-            self.codomain = func(x1, x2)
+    def evaluate(self, func, x, y = None):
+        if y is None:
+            if(self.dim_domain == 1 and self.dim_codomain == 1):
+                self.domain = x
+                self.codomain = func(x)
+            elif(self.dim_domain == 2 and self.dim_codomain == 1):
+                x1, x2 = np.meshgrid(x, x)
+                self.domain = np.meshgrid(x, x)
+                self.codomain = func(x1, x2)
+        else:
+            self.domain = np.meshgrid(x, y)
+            self.codomain = func(self.domain[0], self.domain[1])
 
     def plot(self):
         if(self.dim_domain == 1 and self.dim_codomain == 1):
@@ -27,11 +30,11 @@ class Function():
             plt.show()
         elif(self.dim_domain == 2 and self.dim_codomain == 1):
             from mpl_toolkits.mplot3d import Axes3D
-            # x, y = np.meshgrid(self.domain, self.domain)
-            x, y = self.domain
             fig = plt.figure()
             ax = fig.add_subplot(111, projection='3d')
-            ax.plot_surface(x, y, self.codomain,  rstride=4, cstride=4, color='b')
+            ax.plot_surface(self.domain[0], self.domain[1], self.codomain,  rstride=4, cstride=4, color='b')
+            plt.title(self.functype)
+            plt.xlabel('x'); plt.ylabel('y')
             plt.show()
         else:
             print("\n\tNothing to plot\n")
--- D1-exercise/object_oriented.py
+++ D1-exercise/object_oriented.py
@@ -21,17 +21,25 @@ e.plot()
 
 g = lambda z : np.sin(z)
 
-x = np.linspace(0, 7, 100)
+x = np.linspace(0, 8, 100)
 
 my_f = Function(1, 1)
 my_f.evaluate(g, x)
 my_f.plot()
 
-par = lambda x, y: 10*(x - 2)**2 + 5*(y - 2)**2 + 2. * x - 3. * y + 1
+par = lambda x, y: 5.*(x - 2)**2 + 3.5*(y - 2)**2 + 2. * x - 3. * y - x * y - 2.
 
 three = Function(2, 1)
 three.evaluate(par, x)
 three.functype = "Paraboloid"
 three.plot()
+
+osc = lambda x, y: np.sin(x + y)
+
+y = np.linspace(0, 10, 500)
+new_f = Function(2, 1)
+new_f.evaluate(osc, x, y)
+new_f.functype = "3D Sin"
+new_f.plot()
 
 # For some reason i need here the p1 coefficents...
